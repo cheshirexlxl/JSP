d73b0af1725833a9cc28c408f6f7cfa7b06ab58d
cheshirexlxl <cheshire_xlxl@gmail.com>
2025-12-10 17:58:38 +0900

    1

---
+++ Servlet/src/main/java/encoding/NoEncodingServlet.java
@@ -0,0 +1,32 @@
+package encoding;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/no-encoding")
+public class NoEncodingServlet extends HttpServlet {
+	
+	protected void doGet(
+			HttpServletRequest request, 
+			HttpServletResponse response) throws ServletException, IOException {
+		// 요청
+		String name = request.getParameter("name");
+		response.getWriter().append("name : " + name);
+		// 응답
+		response.getWriter().append("no-encoding : 인코딩 설정 안함");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
