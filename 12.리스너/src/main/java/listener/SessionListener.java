172dc5f572b9dc524f7b845a8583b7472d4f5ef4
cheshirexlxl <cheshire_xlxl@gmail.com>
2025-12-12 17:38:01 +0900

    40일차

---
+++ 12.리스너/src/main/java/listener/SessionListener.java
@@ -0,0 +1,26 @@
+package listener;
+
+import jakarta.servlet.annotation.WebListener;
+import jakarta.servlet.http.HttpSessionEvent;
+import jakarta.servlet.http.HttpSessionListener;
+
+@WebListener
+public class SessionListener implements HttpSessionListener {
+
+    public SessionListener() {
+        System.out.println("세션 리스너 객체 생성");
+    }
+    public void sessionCreated(HttpSessionEvent se)  { 
+    	String sessionId = se.getSession().getId();
+    	se.getSession().setMaxInactiveInterval(60);
+    	System.out.println("세션 생성 - SESSIONID : " + sessionId);
+    	System.out.println("-----------------------------------");  
+      
+    }
+
+    public void sessionDestroyed(HttpSessionEvent se)  { 
+    	String sessionId = se.getSession().getId();
+    	System.out.println("세션 종료 - SESSIONID : " + sessionId);
+    	System.out.println("-----------------------------------");  
+    }	
+}
